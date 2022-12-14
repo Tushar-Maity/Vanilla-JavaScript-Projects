4c4e7e1be9b5a5d8dd797ad87cc68d030927b66f
Tushar985 <tusharmaity989@gmail.com>
2022-12-15 00:46:16 +0530

    New year date updated

--- countdown-timer/scripts.js
+++ countdown-timer/scripts.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYear = "1 Jan 2022";
+const newYear = "1 Jan 2023";
 
 function countdown() {
     const newYearDate = new Date(newYear);
